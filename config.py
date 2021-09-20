e17901ee07338a2dec1c881e97ec18c4d52a526c
Андрей Бучкевич <72127434+Neurosecond@users.noreply.github.com>
2021-09-20 16:38:19 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Config for data pipeline"""
+
+# Created by Buchkevich 12/03/2018
+
+# MySQL config constants
+MYSQL_USERNAME = "root"
+MYSQL_PASSWORD = "1234"
+MYSQL_HOST = "localhost"
+MYSQL_DB = "sampleDB"
+
+# MongoDB config constants
+MONGO_HOST = "localhost"
+MONGO_PORT = 27017
+MONGO_DB = "sampleDB"
